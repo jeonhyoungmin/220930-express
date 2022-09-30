5b6ebe6c71bae9e178cef6ba4a93512e209bfd95
jeonhyoungmin <jhmin7509@gmail.com>
2022-09-30 14:18:11 +0900

    3.3.1 p61

---
+++ ch3/meadowlark/site/meadowlark.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const expressHandlebars = require('express-handlebars')
+
+// 핸들바 뷰 엔진 설정
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req,res)=>{
+  res.type('text/plain')
+  res.send('Meadowlark Travel');
+})
+
+app.get('/about', (req, res) => {
+  res.type('text/plain')
+  res.send('About Meadowlark Travel')
+})
+
+//custom 404 page
+
+app.use((req, res)=> {
+  res.type('text/plain')
+  res.status(404)
+  res.send('404 - Not Found')
+})
+
+// custom 500 page
+app.use((err, req, res, next)=>{
+  console.error(err.message)
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log(
+  `Express started on http://localhost:${port};` +
+  `press Ctrl-C to terminate.`
+))
